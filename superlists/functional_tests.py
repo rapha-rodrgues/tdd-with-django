d200dd711e5995341c6107a2469946673850729e
Raphael Nogueira <raphael0608@gmail.com>
2022-03-22 15:34:59 -0300

    Adiciona mais testes funcionais para validar a entrada de um item na lista de tarefas

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,8 +1,11 @@
+import time
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
-class NewVisitor(unittest.TestCase):
+class NewVisitorTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
@@ -18,19 +21,32 @@ class NewVisitor(unittest.TestCase):
         # Ela percebe que o título da página e o cabeçalho mencionam listas
         # de tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ela é convidada a inserir um item da tarefa imediatamente
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # Ela digita "Buy peacock feathers" (Comprar penas de pavão) em
         # uma caixa de texto (o hobby de Edith é fazer iscas para pesca com fly)
+        inputbox.send_keys('Buy peacock feathers')
 
         # Quando ela tecla enter, a página é atualizada, e agora apágina lista
         # "1: Buy peacock feathers" como um item em uma lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
         # Ela insere "Use peacock feathers to make fly"
         # (Usar penas de pavão para fazer um fly - Edith é bem metódica)
+        self.fail('Finish the test!')
 
         # A página é atualizada novamente e agora mostra os dois itens em sua lista
 
