8c243c4a81d2d0538cf83068bf61c2122928b672
Raphael Rodrigues <raphael0608@gmail.com>
2022-04-03 14:33:57 -0300

    - Adiciona testes funcionais para verificação de estilo e layout

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -118,3 +118,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfeitos ambos voltam a dormir
+
+    def test_layout_and_atyling(self):
+        # Edith acessa a pagina inicial
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Ela percebe que a caixa de entrada esta elegantemente centralizada
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Ela inicia uma nova lista e ve que a entrada esta elegantemente
+        # centralizada tambem
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
