85e5c27fd86c982161e81e14d15713048027f757
Raphael Rodrigues <raphael0608@gmail.com>
2022-04-03 17:08:08 -0300

    - Adiciona o bootstrap ao projeto - Adiciona um arquivo base para estilos customizados de css alem do bootstrap - Refatora o template base para adicionar os novos estilos

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,15 +1,15 @@
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
