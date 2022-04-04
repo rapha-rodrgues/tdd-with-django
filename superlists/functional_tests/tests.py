2787aeb8a24e147f4131ddb9c1c1388a205969a0
Raphael Rodrigues <raphael0608@gmail.com>
2022-04-03 22:48:13 -0300

    Altera os testes funcionais para permitir testes do ambiente de staging

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,3 +1,4 @@
+import os
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -12,6 +13,9 @@ MAX_WAIT = 10
 class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self) -> None:
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = f'http://{staging_server}'
         self.browser = webdriver.Firefox()
 
     def tearDown(self) -> None:
