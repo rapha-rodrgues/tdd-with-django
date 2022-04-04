b4c5a2ed6d52486f5234d8d2c97c1e3f8b3fe867
Raphael Rodrigues <raphael0608@gmail.com>
2022-04-04 00:24:02 -0300

    Adiciona origens seguras para CSRF do Django

--- superlists/settings.py
+++ superlists/settings.py
@@ -125,3 +125,5 @@ STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://*.raphaelnogueira.dev']
